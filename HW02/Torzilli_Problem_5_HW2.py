4ff4332a0f0a2dd603758270913b28f0896c2fe9
Robert Torzilli <torzillirobert@gmail.com>
2017-10-22 19:47:35 -0400

    Probelm 5 all but part b finished(?)

--- HW02/Torzilli_Problem_5_HW2.py
+++ HW02/Torzilli_Problem_5_HW2.py
@@ -5,6 +5,8 @@ Created on Sun Oct 22 15:04:05 2017
 @author: Robert
 """
 import numpy as np
+from scipy.linalg import solve
+import matplotlib.pyplot as plt
 # A]  Construct A and bVector
 n = 100
 # Making bVector
@@ -16,19 +18,38 @@ bVector_tmp = np.concatenate([bVector_start,bVector_mid])
 bVector = np.concatenate([bVector_tmp, bVector_end])
 
 # Make A matrix
-A_matrix_sta = np.array([[0,2,-1]])
 # Create a mold to repeat
-A_matrix_mid_val = np.array([[-1,2,-1]]) 
-A_matrix_mid = np.repeat(A_matrix_mid_val,n-2, axis = 0)
-A_matrix_end = np.array([[0,2,-1]])
-# Combine begining middle and end parts
-A_matrix_tmp = np.concatenate([A_matrix_sta,A_matrix_mid])
-A_matrix = np.concatenate([A_matrix_tmp,A_matrix_end])
-print(len(A_matrix))
-print(len(bVector))
-inverseA = np.invert(A_matrix)
-print(inverseA)
-print(inverseA*A_matrix)
-
+A_matrix = np.zeros((n,n))
+A_matrix[0,0]=2
+A_matrix[0,1]=-1
+# Row Track, don't want to overwrite zeros
+j = 0
+#Column track
+for i in range(1,n-1):
+    if (j<=97):
+        j += 1
+        A_matrix[i,j-1]=-1
+        A_matrix[i,j]=2
+        A_matrix[i,j+1]=-1
+A_matrix[n-1,n-2]=-1
+A_matrix[n-1,n-1]=2
 # B]
 # What is the condition number of A?
+
+
+# C]
+# Explicitly solve the X values using inverse multiplication
+inverseA = np.linalg.inv(A_matrix)
+identityProof = (np.matmul(inverseA,A_matrix))
+# D]
+# Solve the X values using scipy's solve function
+solutionExplicit = np.matmul(inverseA,bVector)
+solutionScipy = solve(A_matrix,bVector)
+
+# Plot the "explicit" and scipy's numerical solutions on the same plot
+plt.title("Found Solutions to the Linear Equation in Prb 5")
+plt.plot(solutionExplicit,bVector,'-ro')
+plt.plot(solutionScipy,bVector,'-b*')
+plt.legend(("Explicit","Scipy"),loc = "center left")
+plt.xlabel("Soultion Values (X)")
+plt.ylabel("Given bVector (Y)")
